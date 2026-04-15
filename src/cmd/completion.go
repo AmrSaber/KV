5a1ee76eb64b264b278eddc0dfe0b0fcb9330e60
Amr Saber <amr.m.saber.mail@gmail.com>
2026-04-16 00:00:00 +0100

    completion: auto detect shell

--- src/cmd/completion.go
+++ src/cmd/completion.go
@@ -1,78 +1,46 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/AmrSaber/kv/src/common"
 	"github.com/spf13/cobra"
 )
 
+var SupportedShells = []string{"bash", "zsh", "fish", "powershell"}
+
 // completionCmd represents the completion command
 var completionCmd = &cobra.Command{
 	Use:   "completion [bash|zsh|fish|powershell]",
 	Short: "Generate shell completion script",
-	Long: fmt.Sprintf(`Generate shell completion script for the specified shell.
+	Long: ` # Auto-detect shell (recommended)
+  eval "$(kv completion)"
 
-Enables auto-completion for commands, flags, and key names.
-
-To load completions:
-
-Bash:
-
-  $ source <(%[1]s completion bash)
-
-  # To load completions for each session, execute once:
-  # Linux:
-  $ %[1]s completion bash > /etc/bash_completion.d/%[1]s
-  # macOS:
-  $ %[1]s completion bash > $(brew --prefix)/etc/bash_completion.d/%[1]s
-
-Zsh:
-
-  # If shell completion is not already enabled in your environment,
-  # you will need to enable it.  You can execute the following once:
-
-  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
-
-  # To load completions for each session, execute once:
-  $ %[1]s completion zsh > "${fpath[1]}/_%[1]s"
-
-  # You will need to start a new shell for this setup to take effect.
-
-fish:
-
-  $ %[1]s completion fish | source
-
-  # To load completions for each session, execute once:
-  $ %[1]s completion fish > ~/.config/fish/completions/%[1]s.fish
-
-PowerShell:
-
-  PS> %[1]s completion powershell | Out-String | Invoke-Expression
-
-  # To load completions for every new session, run:
-  PS> %[1]s completion powershell > %[1]s.ps1
-  # and source this file from your PowerShell profile.
-`, rootCmd.Name()),
-	Example: fmt.Sprintf(`  # Generate bash completion
-  %[1]s completion bash
-
-  # Install bash completion (Linux)
-  %[1]s completion bash > /etc/bash_completion.d/%[1]s
-
-  # Install zsh completion
-  %[1]s completion zsh > "${fpath[1]}/_%[1]s"
-
-  # Install fish completion
-  %[1]s completion fish > ~/.config/fish/completions/%[1]s.fish`, rootCmd.Name()),
+  # Explicit shell:
+  eval "$(kv completion bash)"
+  eval "$(kv completion zsh)"
+  eval "$(kv completion fish)"
+  eval "$(kv completion powershell)"`,
 	DisableFlagsInUseLine: true,
-	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	ValidArgs:             SupportedShells,
+	Args:                  cobra.MatchAll(cobra.MaximumNArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		var err error
 
-		switch args[0] {
+		var shell string
+		if len(args) == 1 {
+			shell = args[0]
+		} else {
+			shell = filepath.Base(os.Getenv("SHELL"))
+
+			if shell == "." {
+				shell = ""
+			}
+		}
+
+		switch shell {
 		case "bash":
 			err = cmd.Root().GenBashCompletion(os.Stdout)
 		case "zsh":
@@ -81,6 +49,12 @@ PowerShell:
 			err = cmd.Root().GenFishCompletion(os.Stdout, true)
 		case "powershell":
 			err = cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+		default:
+			if shell == "" {
+				common.Fail("error: could not detect shell, please pass it explicitly: kv completion <shell>")
+			} else {
+				common.Fail("error: unsupported shell %q, supported: %v", shell, strings.Join(SupportedShells, ", "))
+			}
 		}
 
 		common.FailOn(err)
