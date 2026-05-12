93767ea07b3e3bccd38f38ddef908b275c946bf7
Amr Saber <amr.m.saber.mail@gmail.com>
2026-05-12 22:33:46 +0100

    Update db validation

--- src/common/validation.go
+++ src/common/validation.go
@@ -4,5 +4,6 @@ import "strings"
 
 func ValidateDBName(db string) {
 	Assert(!strings.Contains(db, "@"), "DB name cannot contain '@'")
+	Assert(!strings.Contains(db, "."), "DB name cannot contain '.'")
 	Assert(!strings.Contains(db, " "), "DB name cannot contain spaces")
 }
